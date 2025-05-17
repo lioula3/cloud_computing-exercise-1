7c393728843956cff3d8c66066a8086589b90d39
lioula <lioula@MacBook-Pro-de-lioula.local>
2025-05-17 18:50:15 +0200

    ess2

--- cmd/main.go
+++ cmd/main.go
@@ -187,10 +187,10 @@ func main() {
 	defer cancel()
 
 	// TODO: make sure to pass the proper username, password, and port
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://mongodb:testmongo@localhost:27017"))
 
 	// This is another way to specify the call of a function. You can define inline
-	// functions (or anonymous functions, similar to the behavior in Python)
+	// functions (or anonymous funcstions, similar to the behavior in Python)
 	defer func() {
 		if err = client.Disconnect(ctx); err != nil {
 			panic(err)
